ba5566b8220be497e9fc66e2fdb7e371cc00f66e
jairajrenjith <jairajrenjith@gmail.com>
2025-12-11 16:32:59 +0530

    pgm files

---
+++ src/components/VideoDetail.jsx
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { 
+  Typography, 
+  Box, 
+  Stack, 
+  Button
+} from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+import { Videos, Loader } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+  
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const handleSubscribe = () => {
+    setIsSubscribed(prev => !prev);
+  };
+  
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then((data) => setVideoDetail(data.items[0]))
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+      .then((data) => setVideos(data.items))
+  }, [id]);
+
+  if(!videoDetail?.snippet) return <Loader />;
+
+  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetail;
+
+  return (
+    <Box minHeight="95vh" sx={{ overflowY: 'auto' }}> 
+      <Stack 
+        direction={{ xs: "column", md: "row" }}
+        sx={{ flexDirection: { md: 'row' } }}
+      >
+        
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer 
+              url={`https://www.youtube.com/watch?v=${id}`} 
+              className="react-player" 
+              controls 
+              width='100%' 
+              height='77vh'
+            />
+            
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+            
+            <Stack 
+              direction="row" 
+              justifyContent="space-between" 
+              sx={{ color: "#fff" }} 
+              py={1} 
+              px={2} 
+            >
+              <Link to={`/channel/${channelId}`} style={{ display: 'flex', alignItems: 'center' }}>
+                <Typography 
+                  variant={{ sm: "subtitle1", md: 'h6' }} 
+                  color="#fff" 
+                  fontWeight="bold" 
+                >
+                  {channelTitle}
+                  <CheckCircleIcon sx={{ fontSize: "14px", color: "gray", ml: "5px" }} />
+                </Typography>
+              </Link>
+              
+              <Stack direction="row" gap="20px" alignItems="center">
+                
+                <Button 
+                    onClick={handleSubscribe}
+                    sx={{
+                        backgroundColor: isSubscribed ? 'grey' : '#FC1503',
+                        color: 'white',
+                        borderRadius: '20px',
+                        fontWeight: 'bold',
+                        px: '15px',
+                        py: '5px',
+                        '&:hover': {
+                            backgroundColor: isSubscribed ? 'darkgray' : '#E61300',
+                        }
+                    }}
+                >
+                    {isSubscribed ? 'SUBSCRIBED' : 'SUBSCRIBE'}
+                </Button>
+                
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
+            </Stack>
+            <Box sx={{ borderBottom: '1px solid #3d3d3d', mx: 2, my: 1 }} /> 
+          </Box>
+          
+          
+          
+        </Box>
+
+        <Box 
+          px={2} 
+          py={{ md: 1, xs: 5 }} 
+          justifyContent="center" 
+          alignItems="center" 
+          
+          sx={{
+            width: { md: '30%' }, 
+            p: 2,
+            maxHeight: { md: '90vh' },
+            overflowY: { md: 'auto' }, 
+          }}
+        >
+          <Videos videos={videos} direction="column" />
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default VideoDetail;
